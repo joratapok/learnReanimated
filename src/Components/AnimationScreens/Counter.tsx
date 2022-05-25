a53ad9c7dd87725c3dcd55ef14af76afca418aa9
Paliev Andrey <joratapok@gmail.com>
2022-05-26 00:36:45 +0700

    fix typescript issue

--- src/Components/AnimationScreens/Counter.tsx
+++ src/Components/AnimationScreens/Counter.tsx
@@ -152,7 +152,7 @@ const styles = StyleSheet.create({
     color: '#111',
   },
   counterContainer: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
   },
--- src/Components/AnimationScreens/HorseMystery.tsx
+++ src/Components/AnimationScreens/HorseMystery.tsx
@@ -290,7 +290,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255,0,0,0.3)',
   },
   isUsed: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
     // backgroundColor: 'purple',
   },
 });
